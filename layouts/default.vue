1ca2923008a8a43e62f35c34fa4af5af1882283a
Nikita Usachov <nikitausachov@Nikitas-MacBook-Pro.local>
2025-05-10 23:17:49 +0300

    final

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="container">
+		<header>
+			<h1>Joke generator</h1>
+		</header>
+
+		<NuxtPage />
+	</div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.container {
+	max-width: 600px;
+	margin: 0 auto;
+	padding: 20px;
+}
+
+header {
+	text-align: center;
+	margin-bottom: 20px;
+}
+</style>
